ae8bd49f3ca94cbfd63f61edf66d6ea881400869
ansidev <ansidev@gmail.com>
2015-05-14 23:21:15 +0700

    script xu ly cho media/upload

---
+++ public/js/blog/media.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+    $('#file').change(function () {
+        var file = $('#file').val().replace(/C:\\fakepath\\/i, '');
+        var filename = file.substring(0, file.lastIndexOf('.'));
+        autoTitle(filename);
+    });
+    $('#title').change(function () {
+        autoSlug($('#title').val());
+    });
+});
+
+function autoTitle(filename) {
+    var url = '/media/autoSlug/' + filename;
+    $.ajax({
+        type: "post",  // Request method: post, get
+        url: url, // URL to request
+        success: function (response) {
+            var json = JSON.parse(response);
+            var i = 0;
+            var file = $('#file').val().replace(/C:\\fakepath\\/i, '');
+            var ext = file.substring(file.lastIndexOf('.'), file.length);
+            $('#slug').val(json[0]);
+            $('#title').val(json[0].replace(/-/g, ' '));
+            $('#description').val($('#title').val());
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+    return false;
+}
+
+function autoSlug(title) {
+    var url = '/media/autoSlug/' + title;
+    $.ajax({
+        type: "post",  // Request method: post, get
+        url: url, // URL to request
+        success: function (response) {
+            var json = JSON.parse(response);
+            var i = 0;
+            var file = $('#file').val().replace(/C:\\fakepath\\/i, '');
+            var ext = file.substring(file.lastIndexOf('.'), file.length);
+            $('#slug').val(json[0]);
+            //$(selector).val(json[0].replace(/-/g, ' '));
+            //$('#file-name').val($('#slug').val() + ext);
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+    return false;
+}
