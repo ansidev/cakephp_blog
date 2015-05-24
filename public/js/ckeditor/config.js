1cd1fb035e63aa6e68af409e71bd0b0fcb626c5a
ansidev <ansidev@gmail.com>
2015-05-25 02:15:07 +0700

    Change CKeditor skin, css for editor content, language.

--- public/js/ckeditor/config.js
+++ public/js/ckeditor/config.js
@@ -3,8 +3,19 @@
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+CKEDITOR.editorConfig = function (config) {
+    // Define changes to default configuration here. For example:
+    config.language = 'vi';
+    // config.uiColor = '#AADC6E';
+    //config.skin = 'bootstrapck';
+    config.skin = 'office2013';
+    //config.extraPlugins = 'bootstrap';
+    CKEDITOR.config.contentsCss = '/css/bootstrap.css';
+
+    // get path of directory ckeditor
+    //var basePath = CKEDITOR.basePath;
+    //basePath = basePath.substr(0, basePath.indexOf("ckeditor/"));
+    //(function () {
+    //    CKEDITOR.plugins.addExternal('bootstrap', basePath + 'ckeditor/bootstrap-ckeditor', 'bootstrap-ckeditor.js');
+    //})();
 };
