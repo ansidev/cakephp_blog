d8351b1839f327fdae18160cfc6b77a667f8d751
ansidev <ansidev@gmail.com>
2015-05-27 19:39:16 +0700

    Sua ham echoShortText($body).

--- src/View/Helper/ContentHelper.php
+++ src/View/Helper/ContentHelper.php
@@ -14,7 +14,7 @@ use Cake\View\View;
 class ContentHelper extends Helper
 {
 //    use StringTemplateTrait;
-    public $helpers = ['Html'];
+    public $helpers = ['Html', 'Text'];
 //    protected $_defaultConfig = [
 //        'templates' => [
 //            'listContainer' => '<div class="list-group"{{attrs}}>{{content}}</div>',
@@ -43,30 +43,35 @@ class ContentHelper extends Helper
     public function echoShortText($body, $length = 400)
     {
         $body = strip_tags(html_entity_decode($body));
-        if (empty($body)) {
-            return '';
-        } else {
-            if (strlen($body) <= $length) {
-                return $body;
-            } else {
-//                $pos_arr = array_keys($body_arr);
-//                foreach ($pos_arr as $num) {
-//                    $pos =
-//                }
-                $result = trim(substr($body, 0, $length));
-                $body_arr = $this->str_word_count($result, 2);
-                $key_arr = array_keys($body_arr);
-                $pos = end($key_arr);
-                $pos = prev($key_arr);
-                $pos = strlen($body_arr[$pos]) + $pos;
-                $result = trim(substr($body, 0, $pos));
-//                if (strlen($result) <= $length) {
-//                    return $result;
-//                } else {
-                    return $result . '...' . '<br>';
-//                }
-            }
-        }
+        return $this->Text->truncate($body, $length, [
+            'ending' => '...',
+            'exact'  => true,
+            'html'   => true,
+        ]) . '<br>';
+//        if (empty($body)) {
+//            return '';
+//        } else {
+//            if (strlen($body) <= $length) {
+//                return $body;
+//            } else {
+////                $pos_arr = array_keys($body_arr);
+////                foreach ($pos_arr as $num) {
+////                    $pos =
+////                }
+//                $result = trim(substr($body, 0, $length));
+//                $body_arr = $this->str_word_count($result, 2);
+//                $key_arr = array_keys($body_arr);
+//                $pos = end($key_arr);
+//                $pos = prev($key_arr);
+//                $pos = strlen($body_arr[$pos]) + $pos;
+//                $result = trim(substr($body, 0, $pos));
+////                if (strlen($result) <= $length) {
+////                    return $result;
+////                } else {
+//                    return $result . '...' . '<br>';
+////                }
+//            }
+//        }
 
     }
 }
