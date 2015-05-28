2a6c69ad5cd6c5da526296e6b56bfbee0e5dab01
ansidev <ansidev@gmail.com>
2015-05-29 05:58:44 +0700

    Cap nhat Helper, them ham moi.

--- src/View/Helper/MediaHelper.php
+++ src/View/Helper/MediaHelper.php
@@ -9,6 +9,7 @@ namespace App\View\Helper;
 
 
 use Cake\Core\Configure;
+use Cake\ORM\TableRegistry;
 use Cake\View\Helper;
 
 class MediaHelper extends Helper
@@ -18,5 +19,18 @@ class MediaHelper extends Helper
     public function url($relative_path)
     {
         return Configure::read('App.rootUrl') . h($relative_path);
+    }
+    public function getDescription($media_id) {
+        if ($media_id === null) {
+            return '';
+        }
+        $object = TableRegistry::get('Media');
+        $query = $object->find('all')
+            ->select(['description'])
+            ->where(['Media.id' => $media_id])
+            ->limit(1)
+            ->toArray();
+        return json_decode($query[0]['description'], true)['description'];
+
     }
 }
